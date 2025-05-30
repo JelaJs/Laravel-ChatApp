a4bfb3953c159a9572fd8e711c9050a0b82a1d0b
JelaJs <nikola4434@gmail.com>
2025-05-30 17:41:02 +0200

    * Styled users div

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,17 +8,24 @@
     </x-slot>
 
     <div class="py-12">
-        <div>
-            @foreach($users as $user)
-                @if($user->id !== auth()->id())
-                    <a href="http://127.0.0.1:8000/chat/{{$user->id}}" class="users">{{$user->name}}</a>
-                @endif
-            @endforeach
-        </div>
+
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <div id="chat-body"></div>
+
+                    <div class="flex flex-col space-y-2">
+                        @foreach($users as $user)
+                            @if($user->id !== auth()->id())
+                                <a
+                                    href="http://127.0.0.1:8000/chat/{{$user->id}}"
+                                    class="inline-block bg-blue-200 text-black text-lg font-medium py-2 px-4 rounded-lg shadow hover:bg-blue-300 transition"
+                                >
+                                    {{ $user->name }}
+                                </a>
+                            @endif
+                        @endforeach
+                    </div>
+
                 </div>
             </div>
         </div>
