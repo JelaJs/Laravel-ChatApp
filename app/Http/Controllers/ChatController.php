86d2ced13216c147ff6f44e6d45e5403edc810e8
JelaJs <nikola4434@gmail.com>
2025-06-02 16:18:12 +0200

    * Refactoring * Moved logic back to the controller to increase executing performance * Installed configured redis for caching

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -5,15 +5,24 @@ namespace App\Http\Controllers;
 use App\Events\MessageSent;
 use App\Http\Requests\MessageRequest;
 use App\Models\Chat;
+use App\Models\Conversation;
 use App\Models\User;
-use App\Services\ChatService;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 
 class ChatController extends Controller
 {
-    public function show(ChatService $chatService, User $user)
+    public function show(User $user)
     {
-        $conversation = $chatService->getConversation($user->id);
+        $conversation = $this->getActiveConversation($user->id);
+
+        if(!$conversation) {
+            $conversation = Conversation::create([
+                'sender_id' => Auth::id(),
+                'receiver_id' => $user->id,
+                'conversation_id' => Str::uuid(),
+            ]);
+        }
 
         $chat = Chat::where('conversation_id', $conversation->conversation_id)
             ->orderBy('created_at', 'asc')
@@ -26,11 +35,23 @@ class ChatController extends Controller
         ]);
     }
 
-    public function sendMessage(MessageRequest $request, ChatService $chatService)
+    public function sendMessage(MessageRequest $request)
     {
-        $conversation = $chatService->getConversation($request->receiver_id);
+        $conversation = $this->getActiveConversation($request->receiver_id);
         MessageSent::dispatch($request->message, Auth::id(), $request->receiver_id, $conversation->conversation_id);
 
-        $chatService->createMessage($request->message, $conversation->conversation_id);
+        Chat::create([
+            'sender_id' => Auth::id(),
+            'conversation_id' => $conversation->conversation_id,
+            'message' => $request->message
+        ]);
+    }
+
+    public function getActiveConversation($userId)
+    {
+        $conversation1 = Conversation::where('receiver_id', $userId)->where('sender_id', Auth::id())->first();
+        $conversation2 = Conversation::where('receiver_id', Auth::id())->where('sender_id', $userId)->first();
+
+        return $conversation1 ?? $conversation2;
     }
 }
